a5281c258abcf5b83e27f60c4d9f34aceebca1d9
vkuznetsoff <ss_slavich@mail.ru>
2025-01-02 15:47:21 +0300

    feat: delete card and pagination

--- src/features/cards/componets/Cards/Cards.tsx
+++ src/features/cards/componets/Cards/Cards.tsx
@@ -1,20 +1,32 @@
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, Pagination } from "@mui/material";
 import { nanoid } from "@reduxjs/toolkit";
 import {
   useAddCardMutation,
+  useDeleteCardMutation,
   useGetCardsQuery,
 } from "features/cards/service/cards.api";
 import { ArgCreateCardType } from "features/cards/service/cards.api.types";
-import { MouseEvent, useState } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import s from "./styles.module.css";
 
 export const Cards = () => {
   let { packId } = useParams<{ packId: string }>();
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(5);
 
-  const { data, error, isError, isLoading } = useGetCardsQuery(packId ?? "");
+  const { data, error, isError, isLoading } = useGetCardsQuery({
+    packId: packId ?? "",
+    page,
+    pageCount:2,
+  });
+
+  const [deleteCard] = useDeleteCardMutation()
 
   const [addCard, { isLoading: isAddLoading }] = useAddCardMutation();
+
+ 
 
   if (isLoading || isAddLoading) {
     return <LinearProgress color="secondary" />;
@@ -26,29 +38,64 @@ export const Cards = () => {
   }
   console.log("data: ", data);
 
-  function addCardsHandler() {
+  function addCardHandler() {
     if (packId) {
       const newCard: ArgCreateCardType = {
         cardsPack_id: packId,
-        question: "1. question + " + nanoid(),
-        answer: "2. answer + " + nanoid(),
+        question: "1. question " + nanoid(),
+        answer: "2. answer " + nanoid(),
       };
-      addCard(newCard).unwrap().then( (res) => {
-		const cardQuestion = res.newCard.question
-		toast.success(`Card ${cardQuestion} was added!`)
-	  }).catch(err => {
-		toast.error(err.data.error)
-	  })
+      addCard(newCard)
+        .unwrap()
+        .then((res) => {
+          const cardQuestion = res.newCard.question;
+          toast.success(`Card ${cardQuestion} was added!`);
+        })
+        .catch((err) => {
+          toast.error(err.data.error);
+        });
     }
+  }
+
+  const changePageHandler = (event: ChangeEvent<unknown>, page: number) => {
+  setPage(page)
+  };
+
+  const deleteCardHandler = (cardId: string) => {
+    deleteCard(cardId)
+    
   }
 
   return (
     <div>
       <h1>Cards</h1>
-      <button onClick={addCardsHandler}>Add</button>
-      {data?.cards.map((c) => {
-        return <div>{c.question}</div>;
-      })}
+      <button onClick={addCardHandler}>add card</button>
+
+      <div>
+        {data &&
+          data.cards.map((card) => {
+            
+
+            return (
+              <div className={s.container} key={card._id}>
+                <div>
+                  <b>Question: </b>
+                  <p>{card.question}</p>{" "}
+                </div>
+                <div>
+                  <b>Answer: </b>
+                  <p>{card.answer}</p>{" "}
+                </div>
+                <button onClick={() => deleteCardHandler(card._id)}>DELETE</button>
+              </div>
+            );
+          })}
+      </div>
+      <Pagination
+        count={data && data.cardsTotalCount}
+        onChange={changePageHandler}
+        page={page}
+      />
     </div>
   );
 };
--- src/features/cards/service/cards.api.ts
+++ src/features/cards/service/cards.api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { baseURL } from 'common/api/common.api';
-import { AddCardResponseType, ArgCreateCardType, CardType, FetchCardsResponseType } from './cards.api.types';
+import { AddCardResponseType, ArgCreateCardType, ArgDeleteCardType, ArgGetCardsType, CardType, DeleteCardResponseType, FetchCardsResponseType } from './cards.api.types';
 
 export const cardsApi = createApi({
 	reducerPath: "cardsApi",
@@ -8,18 +8,23 @@ export const cardsApi = createApi({
 		baseUrl: baseURL,
 		credentials: 'include'
 	}),
+	tagTypes: ["Card"],
+	keepUnusedDataFor: 5,
 	endpoints: (build) => {
 		return {
-			getCards: build.query<FetchCardsResponseType, string>({
-				query: (packId) => {
+			getCards: build.query<FetchCardsResponseType, ArgGetCardsType>({
+				query: ({packId, page, pageCount}) => {
 					return {
 						method: "GET",
 						url: "cards/card",
 						params: {
-							cardsPack_id: packId
+							cardsPack_id: packId,
+							page,
+							pageCount
 						}
 					}
-				}
+				},
+				providesTags: ['Card']
 			}),
 
 			addCard: build.mutation<AddCardResponseType,ArgCreateCardType>({
@@ -32,10 +37,25 @@ export const cardsApi = createApi({
 						}
 
 					}
-				}
+				},
+				invalidatesTags: ['Card']
+			}),
+
+			deleteCard: build.mutation<DeleteCardResponseType,string>({
+				query: (id) => {
+					return {
+						method: "DELETE",
+						url: "cards/card",
+						params: {
+						id
+						}
+
+					}
+				},
+				invalidatesTags: ['Card']
 			})
 		}
 	}
 })
 
-export const {useGetCardsQuery, useAddCardMutation} = cardsApi
+export const {useGetCardsQuery, useAddCardMutation, useDeleteCardMutation} = cardsApi
